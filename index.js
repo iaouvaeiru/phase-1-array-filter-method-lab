7c36917ed20b45ed7645dc7ca3d49d36726b182a
Leon Fan <leonkind@gmail.com>
2021-05-01 18:17:13 -0500

    commit tree 295daa6d82fa5ffa1b2ef72f9d2d2a16a18a4e4a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const findMatching = (array, string) => {
+    let matchingNames = array.filter( x => x == string || x == string.toLowerCase())
+    return matchingNames
+}
+
+const fuzzyMatch = (array, string) => {
+    let fuzzyReturn = array.filter(x => x.startsWith(string))
+    return fuzzyReturn
+}
+
+const matchName = (array, string) => {
+    let allMatch = array.filter(x => x.name == string)
+    return allMatch
+}
